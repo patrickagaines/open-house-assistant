3b2fb28c135c6aaeb0acf2d496849e4da63bee1e
Patrick Gaines <patrickagaines@outlook.com>
2023-04-18 11:39:38 -0500

    Adjusted page loader to always show in center of screen regardless of layout

--- src/components/navigation/PageLoader.tsx
+++ src/components/navigation/PageLoader.tsx
@@ -2,8 +2,10 @@ import { PageLoaderIcon } from "../../assets/icons";
 
 export const PageLoader = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <PageLoaderIcon />
+    <div className="fixed bottom-0 left-0 right-0 top-0">
+      <div className="flex h-full w-full items-center justify-center">
+        <PageLoaderIcon />
+      </div>
     </div>
   );
 };
